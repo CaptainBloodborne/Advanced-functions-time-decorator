da9bf2f049490ae2850f880d28a6bc181d9fe899
artem <vrail30@outlook.com>
2021-10-12 22:15:36 +0300

    Task done.

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import functools
 from typing import Dict
+import time
 
 execution_time: Dict[str, float] = {}
 
 
-def time_decorator():
+def time_decorator(func: callable) -> callable:
     """
     Create a decorator function `time_decorator`
     which has to calculate decorated function execution time
     and put this time value to `execution_time` dictionary where `key` is
     decorated function name and `value` is this function execution time.
     """
-    pass
+    start_time = time.time()
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        execution_time[func.__name__] = time.time() - start_time
+        return result
+    return wrapper
